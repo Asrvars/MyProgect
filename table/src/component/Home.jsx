bf5d47915cdd04942e613d258820d913b2b9d190
Asrvars <varsik.asryan75@gmail.com>
2019-09-28 20:04:01 +0400

    Home

--- table/src/component/Home.jsx
+++ table/src/component/Home.jsx
@@ -1,49 +1,42 @@
 import React, { Component } from 'react';
-import Lightbox from 'react-image-lightbox';
-import 'react-image-lightbox/style.css';
-
-const images = [
+import { Slide } from 'react-slideshow-image';
+import  './css/Home.css';
+const slideImages = [
   "https://img2.akspic.ru/image/46291-dikaya_mestnost-ozero-pustynya-boloto-priroda-1920x1080.jpg",
   "https://s1.1zoom.ru/b5050/186/Norway_Mountains_Lake_488324_1920x1080.jpg",
   "http://wallpapers-images.ru/1920x1080/nature/wallpapers/nature-wallpapers-1920x1080-00014.jpg",
   "https://sites.google.com/site/prirodanasevseegooglgfgf/_/rsrc/1463456237313/home/priroda_gory_nebo_ozero_oblaka_81150_1920x1080.jpg",
-  "http://magas.su/sites/magas.su/files/newspics/gori_dzheirah.jpg",
-  "http://top-desktop.ru/files/priroda/1920x1080/156.jpg",
-]
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      photoIndex: 0,
-      isOpen: true,
-    };
-  }
+ 
+];
 
-  render() {
-    const { photoIndex, isOpen } = this.state;
-
-    return (
-      <div>
-        {isOpen && (
-          <Lightbox
-            mainSrc={images[photoIndex]}
-            nextSrc={images[(photoIndex + 1) % images.length]}
-            prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-            onCloseRequest={() => this.setState({ isOpen: false })}
-            onMovePrevRequest={() =>
-              this.setState({
-                photoIndex: (photoIndex + images.length - 1) % images.length,
-              })
-            }
-            onMoveNextRequest={() =>
-              this.setState({
-                photoIndex: (photoIndex + 1) % images.length,
-              })
-            }
-          />
-        )}
-      </div>
-    );
-  }
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true
 }
-export default Home;
+
+class Home extends Component {
+  render(){
+    return (
+      <Slide {...properties}>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[0]})`}}> </div>
+        </div>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[1]})`}}> </div>
+        </div>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[2]})`}}> </div>
+        </div>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[3]})`}}> </div>
+        </div>
+      </Slide>
+    )
+}
+}
+
+export default Home
+
--- table/src/component/Table.jsx
+++ table/src/component/Table.jsx
@@ -18,22 +18,23 @@ class Table extends Component {
 onClickPen = (e) => {
    this.setState({
       isEditMode: true,
-      score: <input type="text" />,
+      score: ""
    })
 }
 
-onClickFaCheckCircle = (e) => {
+onClickFaCheckCircle = (event) => {
 this.setState({
       isEditMode: false,
-     
+      score: event.target.value
    })
 }
 
 
-onChange = (e) => {
-   this.setState({
-      isEditMode: true,
-      score: <input type="trext" />,
+onChange(event){
+
+   this.setState((event) =>{
+ 
+  return{ score: event.target.value} 
    })
 }
 
@@ -51,17 +52,17 @@ renderTableHeader() {
 
 renderTableData() {
     return this.state.data.map((data, index) => {
-      const { id, name, surname, score, } = data //destructuring
+      const { id, name, surname, score,isEditMode } = data //destructuring
           return (
-          <tr  key={this.state.data.id}>
+          <tr key={this.state.index}>
             <td id="td" class="colum">{id}</td>
             <td id="td" >{name}</td>
             <td id="td">{surname}</td>
-            {/* <td id ="td" key ={index}>{this.state.isEditMode ? <input type="text" onChange={this.onChange}  /> : <span>{score}</span>}
-            {this.state.isEditMode ? <button  onClick={this.onChange}><FaCheckCircle /></button> : <button  key={id} onClick={this.changeEditMode}><FaPen/></button>}</td> */}
-            <td id="td">{this.state.isEditMode ? <input type="text" onChange={(e) => this.onChange(id, e)} /> : <span>{score}</span>}
+     {/* <td id ="td" key ={index}>{this.state.isEditMode ? <input type="text" onChange={this.onChange}  /> : <span>{score}</span>}
+            {this.state       .isEditMode ? <button  onClick={this.onChange}><FaCheckCircle /></button> : <button  key={id} onClick={this.changeEditMode}><FaPen/></button>}</td> */}
+            <td id="td">{this.state.isEditMode ? <input type="text" onChange={(event) =>this.onChange(event)} /> : <span>{score}</span>}
 
-               {this.state.isEditMode ? <button key={id} onClick={(e) => this.onClickFaCheckCircle(id, e)}><FaCheckCircle /></button> : <button onClick={(e) => this.onClickPen(id, e)} ><FaPen /></button>}
+               {this.state.isEditMode ? <button key={this.state.id} onClick={ this.onClickFaCheckCircle}><FaCheckCircle /></button> : <button key={this.state.id} onClick={ this.onClickPen} ><FaPen /></button>}
             </td>
          </tr>
       )
